3283746a6a109c7b894f73bcf52b12e790a7e0b4
ChristineMuchiri <tinniemuchiri@gmail.com>
2023-02-16 09:19:44 -0800

    printf function

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * main= prints exact word
+ *
+ * Return= Always (0)
+ */
+int main(void)
+{
+	printf("with proper grammer, but the outcome is a piece of art,\n");
+	return (0);
+}
+
