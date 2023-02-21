9ab8721db8d0e0e359a701cccbed51a09697a8d0
ChristineMuchiri <tinniemuchiri@gmail.com>
2023-02-21 10:18:10 -0800

    printing alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+/**
+  * Main - printing alphabets
+  *
+  * Return: Always 0 (success)
+  */
+
+void print_alphabet(void)
+{
+	char t;
+	{
+	for (t = 'a'; t <= 'z'; t++)
+		putchar(t);
+	}
+		putchar('\n');
+}
