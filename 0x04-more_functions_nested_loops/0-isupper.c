6c2ffd7d82f38e5a88566894258ae60d0b615af6
ChristineMuchiri <tinniemuchiri@gmail.com>
2023-02-23 05:03:42 -0800

    upper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+  * _isupper - check upper or lower case
+  *@c: upper or lower case
+  * Return: (1) if uppercase
+  */
+
+int _isupper(int c)
+{
+	if (_isupper(c))
+	{
+		return ('1');
+	}
+	else
+	{
+		return ('0');
+	}
+}
